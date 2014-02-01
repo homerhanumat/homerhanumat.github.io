5356a82308087a216ae12582596eac646150b1a9
homer <hwhite0@georgetowncollege.edu>
2014-02-01 15:45:42 -0500

    testing rmd post

--- process_R_stuff.R
+++ process_R_stuff.R
@@ -1,12 +1,14 @@
-#This shows how I process octopress website involving R code:
+#This shows how I process octopress website post involving R code:
 
 #in home directory:
 source("~/octopress/rmarkdown.r")
 
-#for posts, change wd to _posts, then:
+#for posts, change wd to RMD_sources, then:
 convertRMarkdown()
-system("cp -r figure ../images")
+system("cp -r figure ../source/images")
 #If there is a cache file, do the same for it.
-#Note that one should name all code chunks for graphs, as all R posts pis in the same figure pot.
 
-#I can keep the .Rmd file in the _posts
+system("cp convertedfile.md ../source/_posts")
+
+
+#Note that one should name all code chunks for graphs, as all R posts pis in the same figure pot.
