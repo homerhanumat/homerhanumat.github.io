9fa4f0612e8032a34f8538779642641042d6f762
homer <hwhite0@georgetowncollege.edu>
2014-02-01 20:27:28 -0500

    chisq test post

--- process_R_stuff.R
+++ process_R_stuff.R
@@ -1,5 +1,12 @@
 #This shows how I process octopress website post involving R code:
 
+#If a toc is desired, place this in document:
+
+# * list element with functor item
+# {:toc}
+
+# styling for toc is in sass/custom/_styles.scss
+
 #in home directory:
 source("~/octopress/rmarkdown.r")
 
@@ -8,7 +15,7 @@ convertRMarkdown()
 system("cp -r figure ../source/images")
 #If there is a cache file, do the same for it.
 
-system("cp convertedfile.md ../source/_posts")
+system("cp 2014-02-01-test.markdown ../source/_posts")
 
 
 #Note that one should name all code chunks for graphs, as all R posts pis in the same figure pot.
