474a99d34b34c815084db4a34f05056b3a9a7f84
homer <hwhite0@georgetowncollege.edu>
2014-02-27 17:32:51 -0500

    add ttestGC tutorial

--- process_R_stuff.R
+++ process_R_stuff.R
@@ -15,7 +15,7 @@ convertRMarkdown()
 system("cp -r figure ../source/images")
 #If there is a cache file, do the same for it.
 
-system("cp 2014-02-21-barchartGCtutorial.markdown ../source/_posts")
+system("cp 2014-02-26-pbinomGCtutorial.markdown ../source/_posts")
 
 
-#Note that one should name all code chunks for graphs, as all R posts pis in the same figure pot.
+#Note that one should name all code chunks for graphs, as all R posts piss in the same figure pot.
