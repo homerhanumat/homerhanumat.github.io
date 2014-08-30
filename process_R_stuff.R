5a919b75d23a83a236082e461398a04867893b40
Homer White <hwhite0@georgetowncollege.edu>
2014-08-30 06:49:05 -0400

    saved the process_r_stuff file

--- process_R_stuff.R
+++ process_R_stuff.R
@@ -8,14 +8,14 @@
 # styling for toc is in sass/custom/_styles.scss
 
 #in home directory:
-source("~/octopress/rmarkdown.r")
+source("~/octopress/octopress/rmarkdown.r")
 
 #for posts, change wd to RMD_sources, then:
 convertRMarkdown()
 system("cp figure/* ../source/images/figure")
 #If there is a cache file, do the same for it.
 
-system("cp 2014-05-05-reasons-to-teach-with-R-3.markdown ../source/_posts")
+system("cp 2014-08-28-r-studio-server-centos-pandoc-latex.markdown ../source/_posts")
 
 
 #Note that one should name all code chunks for graphs, as all R posts piss in the same figure pot.
